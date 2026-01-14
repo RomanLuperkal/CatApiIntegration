2c23104ad4fe2e105f2d989278e441644fdf81bb
Roman <madara-09@bk.ru>
2026-01-14 13:53:50 +0300

    feat: добавлен каркас проекта

---
+++ CatApiIntegration/src/test/java/org/blog/catapiintegration/CatApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blog.catapiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
